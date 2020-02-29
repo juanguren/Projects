e0ea9816e382c7ef20f2b45004d37ee6b5936b2e
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-02-29 12:40:55 -0500

    Create main.js
    Odd & even number game.
    using:
    arrow functions
    arrays
    loops
    try catch

---
+++ JS/JS_Even & Odd/main.js
@@ -0,0 +1,53 @@
+
+/**
+ * This program asks the user X ammount of numbers until a 0 is received. After, it stores everything in an array, deletes that last 0
+ * and divides the original array into two separate ones, filtering the even from the odd values.
+ * Finally it compares both arrays to find out its length and logs who is the winner, taking into account ties too.
+ */
+
+var ask;
+let numbers = [];
+
+compareNumbers = (a) =>{
+    do{
+        ask = parseInt(prompt("Enter your number"));
+        numbers.push(ask);
+
+    } while(ask != 0);
+    numbers.pop();
+}
+
+let i = 0;
+var evenNumbers = [];
+var oddNumbers = [];
+
+calcOddEven = () =>{
+    for(i; i<numbers.length; i++){
+        if (numbers[i] % 2 == 0) {
+            evenNumbers.push(numbers[i]);
+        } else if(numbers[i] % 2 !== 0){
+            oddNumbers.push(numbers[i]);
+        }
+    }
+}
+
+try {
+    compareNumbers(ask);
+    calcOddEven();
+
+    if (evenNumbers.length > oddNumbers.length) {
+        alert("Evens win");
+    } else if(evenNumbers.length < oddNumbers.length){
+        alert("Odds win");
+    } else if(evenNumbers.length === oddNumbers.length){
+        alert("TIE");
+    } else{
+        throw new Error ("something wrong");
+    }
+
+} catch (error) {
+    console.log(error);
+}
+
+console.log("even numbers are: " + evenNumbers);
+console.log("Odd numbers are: " + oddNumbers);
