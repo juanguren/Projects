0fe652ef5978ebd40a7cbf1aaf8b83959cc945b6
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-03-06 16:26:56 -0500

    Error handling
    Try catch + successful or unsuccesful messages

--- JS/List Exercise/main.js
+++ JS/List Exercise/main.js
@@ -5,6 +5,7 @@ var firstName = document.querySelector("#firstName");
 var lastName = document.querySelector("#lastName");
 var email = document.querySelector("#email");
 var phone = document.querySelector("#phone");
+var success = document.querySelector("#successMessage");
 
 //
 var ul = document.querySelector("#showUserList");
@@ -30,35 +31,48 @@ userForm.addEventListener("submit", addUsers);
 
 function addUsers(e) {
     e.preventDefault();
+
+    if (firstName.value === "" || lastName.value === "" || email.value === "" | phone.value === "") {
+        success.innerHTML = "<h5>Unsuccessful</h5>" + `(`+e.message+`)`;
+        throw new Error ("Fields are empty");
+    } else{
+        success.innerHTML = "<h4>Successful!</h4>";
     // text nodes for every user input
-    userInput1 = document.createTextNode(firstName.value);
-    userInput2 = document.createTextNode(lastName.value);
-    userInput3 = document.createTextNode(email.value);
-    userInput4 = document.createTextNode(phone.value);
+        userInput1 = document.createTextNode(firstName.value);
+        userInput2 = document.createTextNode(lastName.value);
+        userInput3 = document.createTextNode(email.value);
+        userInput4 = document.createTextNode(phone.value);
+        // <li> tag for every input field
+        newLi1 = document.createElement("li");
+        newLi2 = document.createElement("li");
+        newLi3 = document.createElement("li");
+        newLi4 = document.createElement("li");
+        // append text to every <li>
+        newLi1.appendChild(userInput1);
+        newLi2.appendChild(userInput2);
+        newLi3.appendChild(userInput3);
+        newLi4.appendChild(userInput4);
+        /*ul.appendChild(newLi1); 
+        ul.appendChild(newLi2); 
+        ul.appendChild(newLi3); 
+        ul.appendChild(newLi4); */
 
-    newLi1 = document.createElement("li");
-    newLi2 = document.createElement("li");
-    newLi3 = document.createElement("li");
-    newLi4 = document.createElement("li");
-    
-    newLi1.appendChild(userInput1);
-    newLi2.appendChild(userInput2);
-    newLi3.appendChild(userInput3);
-    newLi4.appendChild(userInput4);
+        console.log(users);
 
-    /*ul.appendChild(newLi1); 
-    ul.appendChild(newLi2); 
-    ul.appendChild(newLi3); 
-    ul.appendChild(newLi4); */
+        users.push(new Users(firstName.value, lastName.value, email.value, phone.value));
 
-    console.log(users);
-
-    users.push(new Users(firstName.value, lastName.value, email.value, phone.value));
-
-    localStorage.setItem("users", JSON.stringify(users));   
-    //var find = JSON.parse(localStorage.getItem("users"));
+        localStorage.setItem("users", JSON.stringify(users));   
+        //var find = JSON.parse(localStorage.getItem("users"));
+    }
     
 } 
+
+try {
+
+} catch (error) {
+    console.log(error);
+    
+}
     
     
     
