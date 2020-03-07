1ffe4ae062e28f7ca2c0c03e4b3baf10ecafa1d4
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-03-07 16:01:31 -0500

    Better code
    Simplifyied (typo) code for preventing multiple events.

--- JS/List Exercise/products_module/script.js
+++ JS/List Exercise/products_module/script.js
@@ -1,8 +1,18 @@
 
 var userLink = document.querySelector("#usersClick");
-
-window.addEventListener("click", userClick);
+userLink.addEventListener("click", userClick);
 
 function userClick (e) {
     userLink.style.color = "crimson";
+}
+
+var productCatalog = document.querySelector(".product-container");
+productCatalog.addEventListener("click", orderProduct);
+
+function orderProduct(e) {
+    if (e.target !== e.currentTarget) {
+        var product = e.target.id;
+        console.log(product);
+        
+    }  e.stopPropagation();
 }
