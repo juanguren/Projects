60123983b376aae6a13c06b460d9537f3a935433
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-03-12 11:36:52 -0500

    Update index.js
    Cut DOM code for cleaning up users file, moved it to the orders_module

--- JS/List Exercise/users_module/index.js
+++ JS/List Exercise/users_module/index.js
@@ -46,29 +46,6 @@ function addUsers(e) {
 
         localStorage.setItem("users", JSON.stringify(users));   
         //var find = JSON.parse(localStorage.getItem("users"));
-    /*
-        // text nodes for every user input
-        userInput1 = document.createTextNode(firstName.value);
-        userInput2 = document.createTextNode(lastName.value);
-        userInput3 = document.createTextNode(email.value);
-        userInput4 = document.createTextNode(phone.value);
-
-        // <li> tag for every input field
-        newLi1 = document.createElement("li");
-        newLi2 = document.createElement("li");
-        newLi3 = document.createElement("li");
-        newLi4 = document.createElement("li");
-
-        // append text to every <li>
-        newLi1.appendChild(userInput1);
-        newLi2.appendChild(userInput2);
-        newLi3.appendChild(userInput3);
-        newLi4.appendChild(userInput4);
-
-        ul.appendChild(newLi1); 
-        ul.appendChild(newLi2); 
-        ul.appendChild(newLi3); 
-        ul.appendChild(newLi4); */
     } 
 } 
 
