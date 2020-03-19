828b04e5921f9f9c96c56e60c8aefd1ffc9f715e
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-03-18 19:44:21 -0500

    Local Data
    Get user and its products data from local, in order for it to show in the table.

--- Own/JS/Ordering App/orders_module/orders.js
+++ Own/JS/Ordering App/orders_module/orders.js
@@ -1,34 +1,35 @@
 
+// bring data from local storage
+let users = JSON.parse(localStorage.getItem("users"));
+let prices = JSON.parse(localStorage.getItem("prices")); 
 
-/*
-    // text nodes for every user input
-    userInput1 = document.createTextNode(firstName.value);
-    userInput2 = document.createTextNode(lastName.value);
-    userInput3 = document.createTextNode(email.value);
-    userInput4 = document.createTextNode(phone.value);
+// instatiate html elements
+let button = document.querySelector("#orders-display");
+let table = document.querySelector("#order-table");
 
-    // <li> tag for every input field
-    newLi1 = document.createElement("li");
-    newLi2 = document.createElement("li");
-    newLi3 = document.createElement("li");
-    newLi4 = document.createElement("li");
+//variables for holding certain values..
+let text;
 
-    // append text to every <li>
-    newLi1.appendChild(userInput1);
-    newLi2.appendChild(userInput2);
-    newLi3.appendChild(userInput3);
-    newLi4.appendChild(userInput4);
+//event function
+let showOrder = (e) =>{
+    console.log(e);
+    text = document.createTextNode();
+    console.log(text);
+}
 
-    ul.appendChild(newLi1); 
-    ul.appendChild(newLi2); 
-    ul.appendChild(newLi3); 
-    ul.appendChild(newLi4);
+//event that captures button click
+button.addEventListener("click", showOrder);
+
+
+
+
     
 
-    var find = JSON.parse(localStorage.getItem("users"));
+
+/*    
 
     for(let i = 0; i<find.length; i++){
-    for(key in find[i]){
+    for(key in users[i]){
         console.log(find[i][key]);
     }
 }
