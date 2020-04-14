28eaa90cc64c34f1b53f34772ff9e917d25e7a96
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-04-14 17:14:48 -0500

    Financial App
    Display financial information via https://financialmodelingprep.com API
    Stock and crypto

---
+++ Own/JS/API Practice/main.js
@@ -0,0 +1,29 @@
+
+
+/**
+ * TODO
+ *      First Iteration: Display financial info of 3 given assets (two stocks, one crypto). Click event on image
+ *      Second Iteration: Test out charts.js for integrating it into the first
+ *      Third Iteration: Search queries
+ * 
+ * @param {string} url 
+ */
+
+async function testStock(url) {
+    let data = await fetch(url);
+    let find = await data.json();
+    if (find.status == 404) {
+        throw new Error("Something might be wrong with the URL");
+    } else{
+        let quoteAAPL = find[0];
+        console.log(quoteAAPL.symbol + " | " + quoteAAPL.price);
+    }
+}
+
+try {
+    testStock("https://financialmodelingprep.com/api/v3/quote/AAPL");
+} catch (error) {
+    console.log(error);
+}
+
+
