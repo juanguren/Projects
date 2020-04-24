9dc70489b135249c7d87fcb54a3b9321744cd0b8
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-04-23 21:19:54 -0500

    TODOs
    Updated todo´s with a fourth, experimental one.

--- Own/JS/Market App/main.js
+++ Own/JS/Market App/main.js
@@ -5,6 +5,7 @@
  *      First Iteration: Display financial info of 3 given assets (two stocks, one crypto). Click event on image
  *      Second Iteration: Test out charts.js for integrating it into the first
  *      Third Iteration: Search queries
+        Fourth: Integrate https://animejs.com/ (Animation skills)
  * 
  * @param {string} url 
  */
