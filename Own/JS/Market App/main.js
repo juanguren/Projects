39cc171cf40c2d8e6bbf413e616bc58db72d7850
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-04-30 21:00:56 -0500

    Update main.js

--- Own/JS/Market App/main.js
+++ Own/JS/Market App/main.js
@@ -5,8 +5,7 @@
  *      First Iteration: Display financial info of 3 given assets (two stocks, one crypto). Click event on image
  *      Second Iteration: Test out charts.js for integrating it into the first
  *      Third Iteration: Search queries
-        
-        
+ 
         Tools: 
                 1. Use Postman to check both APIs.
                 (optional) Integrate https://animejs.com/ (Animation)
