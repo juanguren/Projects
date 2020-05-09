53030f8632215d5f07ff6a519278ec981614ff23
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-05-09 17:55:33 -0500

    TODO
    Set it just as a TODO file, for now.

--- Own/JS/Market App/main.js
+++ Own/JS/Market App/main.js
@@ -1,5 +1,4 @@
 
-
 /**
  * TODO
  *      First Iteration: Display financial info of 3 given assets (two stocks, one crypto). Click event on image
@@ -9,25 +8,6 @@
         Tools: 
                 1. Use Postman to check both APIs.
                 (optional) Integrate https://animejs.com/ (Animation)
- * 
- * @param {string} url 
  */
-
-async function testStock(url) {
-    let data = await fetch(url);
-    let find = await data.json();
-    if (find.status == 404) {
-        throw new Error("Something might be wrong with the URL");
-    } else{
-        let quoteAAPL = find[0];
-        console.log(quoteAAPL.symbol + " | " + quoteAAPL.price);
-    }
-}
-
-try {
-    testStock("https://financialmodelingprep.com/api/v3/quote/AAPL");
-} catch (error) {
-    console.log(error);
-}
 
 
