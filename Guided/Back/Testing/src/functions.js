d949c9b066ea14306c7f47b4b0e7124a5e5d5f10
juanguren <juan_fe_7@live.com>
2021-02-13 18:04:03 -0500

    Fixed 2nd test

--- Guided/Back/Testing/src/functions.js
+++ Guided/Back/Testing/src/functions.js
@@ -7,6 +7,33 @@ const returnSum = (a, b) =>{
     }
 }
 
+const backWord = (word) =>{
+    const splited = word.split("");
+    const reversedSplit = splited.reverse();
+    return reversedSplit.join("");
+}
+
+const execute = (param) =>{
+    return {
+        param
+    }
+}
+
+const obj = {
+    data: (param) => execute(param)
+}
+
+const objectTest = (param) =>{
+    const result = obj['data'](param);
+    return result
+}
+
 module.exports = {
-    returnSum
+    backWord,
+    objectTest
+};
+
+module.exports = {
+    returnSum,
+    backWord
 };
